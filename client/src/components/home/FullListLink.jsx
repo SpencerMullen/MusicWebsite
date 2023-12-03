77fbff861938c378f02026b11e2d479f3babccd6
SpencerMullen <spencermullen0@gmail.com>
2023-12-03 04:54:37 -0500

    Add user manually fix

--- client/src/components/home/FullListLink.jsx
+++ client/src/components/home/FullListLink.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-import { register } from '../utils/requestUtils';
+import { register } from '../../utils/requestUtils';
 
 export default function FullListLink({ userStatus }) {
   const containerStyle = {
