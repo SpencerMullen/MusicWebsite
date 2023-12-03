012b05bf5a6a0ff445ebd82374507a427b9712b3
SpencerMullen <spencermullen0@gmail.com>
2023-12-03 06:17:40 -0500

    Secure cookie false

--- server/app.js
+++ server/app.js
@@ -71,7 +71,7 @@ app.use(session({
   saveUninitialized: false,
   store: store,
   cookie: { 
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     maxAge: 1000 * 60 * 60 * 24 // 1 day
   }
 }));
