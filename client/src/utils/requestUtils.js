77d6ae5db039886b6f7bf56870f7ac8db7a6fdba
SpencerMullen <spencermullen0@gmail.com>
2023-12-03 01:39:56 -0500

    Trying to hardcode api url

--- client/src/utils/requestUtils.js
+++ client/src/utils/requestUtils.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV !== 'production') {
     API_URL = process.env.BACKEND_URL;
 }
 
+API_URL = 'https://music-list-backend-qwa5.onrender.com/';
+
 /* Entry API calls */
 
 // Get all entries
