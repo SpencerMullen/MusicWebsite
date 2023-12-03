fdd4617dc58337129ca279441ee4b0fcb69a94d1
SpencerMullen <spencermullen0@gmail.com>
2023-12-03 05:13:10 -0500

    Forgot to import user model

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const User = require('../models/user');
 
 // Register route
 router.post('/register', async (req, res) => {
