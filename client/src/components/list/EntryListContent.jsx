e32f66e5b5d4450c3fe7452e322cba9d114eb781
SpencerMullen <spencermullen0@gmail.com>
2023-12-27 17:28:09 -0500

    Try use loading state for scroll position

--- client/src/components/list/EntryListContent.jsx
+++ client/src/components/list/EntryListContent.jsx
@@ -1,12 +1,22 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import EntryCard from './EntryCard';
 
 const EntryListContent = (props) => {
   const { entries, scrollPosition } = props;
 
+  // Used to set scroll position on initial load
+  const [loaded, setLoaded] = useState(false);
+
   // Scroll to previous position
   useEffect(() => {
-    window.scrollTo(0, scrollPosition);
+    if(loaded) {
+      window.scrollTo(0, scrollPosition);
+    }
+  }, [loaded, scrollPosition]);
+
+  // Set loaded to true after first render
+  useEffect(() => {
+    setLoaded(true);
   }, [entries]);
 
   return (
